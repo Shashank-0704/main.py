a6c4599f46063e402e9066ffcda0e5d807736268
Shashank <shashankshukla627@gmail.com>
2025-02-20 04:52:00 -0800

    Learned

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print('''Twinkle twinkle little star
       how are wonder what you are
-      up above the world so high''')
+      up above the world so high
+      world above the world so high''')
