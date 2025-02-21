666578922bb8c36bcf15e50ab5b6a153b5caf56d
Shashank <shashankshukla627@gmail.com>
2025-02-21 05:51:11 -0800

    new branch work

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print('''Twinkle twinkle little star
       how are wonder what you are
       up above the world so high
       world above the world so high''')
-      a = 12
-      b = 14
-      print (a + b)
-      
+a = 12
+b = 14
+print (a + b)
+print("for side branch")
