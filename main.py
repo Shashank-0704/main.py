d877b92ca52f28ce553e5d96836befed356f86f1
Shashank-0704 <shashankshukla627@gmail.com>
2025-02-24 05:36:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 print('''Twinkle twinkle little star
       how are wonder what you are
       up above the world so high
-      world above the world so high''')
+      world above the world so high''')  {//im smart}
+
 a = 12
 b = 14
 print (a + b)
